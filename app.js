dfc008e89159c46cf48bab0cbda017480ca5503c
Cody-KC <ckokafor1992@gmail.com>
2020-12-16 00:12:25 +0100

    primitive/reference type in js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log('hello world')
+
+//primitive value
+
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+console.log (`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+scoreOne = 100;
+
+console.log (`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+//reference value
+
+const userOne = {name: 'cody', age: 28};
+const userTwo = userOne;
+
+console.log(userOne,userTwo);
+
+userOne.age = 30;
+
+console.log(userOne,userTwo);
