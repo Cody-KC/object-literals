b9297347d4eb6983cc3edcdb4168db8a16b79457
Cody-KC <ckokafor1992@gmail.com>
2020-12-16 01:02:55 +0100

    DOM

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-console.log('hello world')
+// console.log('hello world')
 
-//primitive value
+// //primitive value
 
-let scoreOne = 50;
-let scoreTwo = scoreOne;
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
 
-console.log (`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+// console.log (`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
 
-scoreOne = 100;
+// scoreOne = 100;
 
-console.log (`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+// console.log (`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
 
-//reference value
+// //reference value
 
-const userOne = {name: 'cody', age: 28};
-const userTwo = userOne;
+// const userOne = {name: 'cody', age: 28};
+// const userTwo = userOne;
 
-console.log(userOne,userTwo);
+// console.log(userOne,userTwo);
 
-userOne.age = 30;
+// userOne.age = 30;
 
-console.log(userOne,userTwo);
+// console.log(userOne,userTwo);
+
+//INTERACT WITH THE BROWSER
+
+//DOCUMENT OBJECT MODEL (DOM)
+// query selectors are used to grab elements in the HTML
+const para = document.querySelector('body > h1');
+
+console.log(para);
+
+//grabbing multiple elememts
+
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach (para => {
+//     console.log(para)
+// });
+// console.log(paras[2]);
+
+const errors = document.querySelectorAll(".error");
+
+console.log(errors);
